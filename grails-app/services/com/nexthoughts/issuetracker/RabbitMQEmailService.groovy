2e27c8a9b3ac725a3889b9b06f070f1fe3d7c173
Vijay <vshukla684@gmail.com>
2016-09-24 20:58:06 +0530

    Refactor code

--- grails-app/services/com/nexthoughts/issuetracker/RabbitMQEmailService.groovy
+++
@@ -1,9 +0,0 @@
-package com.nexthoughts.issuetracker
-
-import grails.transaction.Transactional
-
-@Transactional
-class RabbitMQEmailService {
-
-
-}
--- grails-app/services/issuetracker/RabbitMqEmailService.groovy
+++ grails-app/services/issuetracker/RabbitMqEmailService.groovy
@@ -24,8 +24,6 @@ class RabbitMqEmailService {
             mailCO.viewFileName = "/emailTemplates/signupEmail"
             mailCO.createdBy = user
             mailService.sendSimpleMailWithoutAttachment(mailCO)
-
-            Notification notification = new Notification(mailCO)
         }
     }
 
