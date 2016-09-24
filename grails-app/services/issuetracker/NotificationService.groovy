8206344c923f8d1b8cc545104ce4d8070199c144
Vijay <vshukla684@gmail.com>
2016-09-24 20:44:09 +0530

    add notifyto field

--- grails-app/services/issuetracker/NotificationService.groovy
+++ grails-app/services/issuetracker/NotificationService.groovy
@@ -4,14 +4,14 @@ import com.User
 import com.nexthoughts.issuetracker.Repository
 import com.nexthoughts.issuetracker.enums.NotificationType
 import com.nexthoughts.notification.Notification
-import grails.transaction.Transactional
 
-@Transactional
 class NotificationService {
+
+    static transactional = false
 
     Notification createNotifiction(Repository repository, User createdBy, NotificationType notificationType) {
         Notification notification = new Notification(createdBy: createdBy, type: notificationType)
-        notification.notifyTo = []
+        notification.notifyTo = User.getTeamMembersByRepository(repository?.id)
         notification.save(flush: true)
         return notification
     }
