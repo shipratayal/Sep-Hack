b27fa982825202d7b753ffe8d5fdd1a65263af42
Nakul Ashish Pant <nakul@nexthoughts.com>
2016-09-24 12:38:59 +0530

    Signup Mail Service

---
+++ test/unit/com/nexthoughts/issuetracker/rabbitmq/consumer/SignupMailServiceSpec.groovy
@@ -0,0 +1,20 @@
+package com.nexthoughts.issuetracker.rabbitmq.consumer
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(SignupMailService)
+class SignupMailServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
