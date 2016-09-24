606af74c078f7f3e54ebdd2d0775572217fcc782
Nakul Ashish Pant <nakul@nexthoughts.com>
2016-09-24 11:56:20 +0530

    Updated bootstrap service for user

--- grails-app/services/issuetracker/BootStrapService.groovy
+++ grails-app/services/issuetracker/BootStrapService.groovy
@@ -27,21 +27,21 @@ class BootStrapService {
         Role adminRole = Role.findByAuthority("ROLE_ADMIN")
         if (!User.countByUsername("admin1@nexthoughts.com")) {
             println " ========== Adding Admin 1 ========="
-            User admin1 = new User(username: "admin1@nexthoughts.com", password: 'admin123')
+            User admin1 = new User(username: "admin1@nexthoughts.com", password: 'admin123',firstName: "Neon",lastName: "Admin")
             admin1.save(flush: true)
             UserRole userRole = new UserRole(user: admin1, role: adminRole)
             userRole.save(flush: true)
         }
         if (!User.countByUsername("admin2@nexthoughts.com")) {
             println " ========= Adding Admin 2 ========="
-            User admin2 = new User(username: "admin2@nexthoughts.com", password: 'admin123')
+            User admin2 = new User(username: "admin2@nexthoughts.com", password: 'admin123',firstName: "Frodo",lastName: "Admin")
             admin2.save(flush: true)
             UserRole userRole = new UserRole(user: admin2, role: adminRole)
             userRole.save(flush: true)
         }
         if (!User.countByUsername("admin3@nexthoughts.com")) {
             println " ========= Adding Admin 3 ========="
-            User admin3 = new User(username: "admin3@nexthoughts.com", password: 'admin123')
+            User admin3 = new User(username: "admin3@nexthoughts.com", password: 'admin123',firstName: "Logan",lastName: "Admin")
             admin3.save(flush: true)
             UserRole userRole = new UserRole(user: admin3, role: adminRole)
             userRole.save(flush: true)
@@ -52,21 +52,21 @@ class BootStrapService {
         Role adminRole = Role.findByAuthority("ROLE_USER")
         if (!User.countByUsername("user1@nexthoughts.com")) {
             println " ========= Adding User 1 ==========="
-            User user1 = new User(username: "user1@nexthoughts.com", password: 'user123')
+            User user1 = new User(username: "user1@nexthoughts.com", password: 'user123',firstName: "Kevin",lastName: "Leon")
             user1.save(flush: true)
             UserRole userRole = new UserRole(user: user1, role: adminRole)
             userRole.save(flush: true)
         }
         if (!User.countByUsername("user2@nexthoughts.com")) {
             println " ========= Adding User 2 ==========="
-            User user2 = new User(username: "user2@nexthoughts.com", password: 'user123')
+            User user2 = new User(username: "user2@nexthoughts.com", password: 'user123',firstName: "Dwayne",lastName: "Johnson")
             user2.save(flush: true)
             UserRole userRole = new UserRole(user: user2, role: adminRole)
             userRole.save(flush: true)
         }
         if (!User.countByUsername("user3@nexthoughts.com")) {
             println " =========== Adding User 3 ==========="
-            User user3 = new User(username: "user3@nexthoughts.com", password: 'user123')
+            User user3 = new User(username: "user3@nexthoughts.com", password: 'user123',firstName: "Matt",lastName: "Morgan")
             user3.save(flush: true)
             UserRole userRole = new UserRole(user: user3, role: adminRole)
             userRole.save(flush: true)
